7da2109a9483b63db57627b6f5824c67b1de3150
Prasad6919 <prasad@machine.tech>
2023-08-19 20:26:43 +0530

    added new info

--- frontend/src/Show.js
+++ frontend/src/Show.js
@@ -1,55 +1,4 @@
 
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
-
-// const App = () => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch data from API
-//     axios.get('/api/data')
-//       .then(response => {
-//         setData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching data:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Data</h2>
-//       <ul>
-//         {data.map(item => (
-//           <li key={item._id}>{item.data.io.di1}</li>
-//         ))}
-//       </ul>
-     
-//       <div>
-//         <h2>Bar Chart</h2>
-//         <BarChart width={600} height={300} data={data}>
-//           <XAxis  />
-//           <YAxis />
-//           <Tooltip />
-//           <Legend
-//     payload={
-//       data.map(item => ({
-//         id: item.data.io.di1,
-//         type: "square",
-//         value: `Di1: ${item.data.io.di1}`,
-//       }))
-//     }
-//   />
-//           <CartesianGrid fill="yellow"/>
-//           <Bar dataKey="data.io.di1" fill="#82ca9d" />
-//         </BarChart>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
@@ -59,15 +8,28 @@ const Show = () => {
 
   useEffect(() => {
     // Fetch data from API
-    axios.get('http://localhost:5000/api/data')
+  //   axios.get('http://localhost:5000/api/data')
+  //     .then(response => {
+  //       setData(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching data:', error);
+  //     });
+  // }, []);
+  axios.get('http://localhost:5000/api/data')
       .then(response => {
-        setData(response.data);
+        // Filter out documents with "modbus" and keep only "io"
+        const filteredData = response.data.filter(item => item.data && item.data.msg === 'io');
+        const processedData = filteredData.map(item => ({
+          di1Value: item.data.io.di1
+        }));
+        setData(processedData);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
-  
+ 
 
   return (
     <div>
@@ -81,7 +43,7 @@ const Show = () => {
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <h2>Bar Chart</h2>
         <BarChart width={1000} height={70} data={data} barCategoryGap={0} padding-left="20px">
-        {/* <XAxis dataKey="_id"  /> */}
+        {/* <XAxis dataKey="name"  /> */}
           {/* <YAxis hide="true" /> */}
           <Tooltip
             contentStyle={{
@@ -93,7 +55,7 @@ const Show = () => {
           />
          
           <CartesianGrid fill="green" stroke='pale green'/>
-          <Bar dataKey="data.io.di1" fill=" yellow" />
+          <Bar dataKey="di1Value" fill=" yellow" />
         </BarChart>
       </div>
     </div>
